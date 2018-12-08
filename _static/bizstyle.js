c5b2c622394189d78b1949a16f54b63438546d9d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-08 21:48:15 +0000

    Deploy nackjicholson/aiosql to github.com/nackjicholson/aiosql.git:gh-pages

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("aiosql 2.0.1 documentation");
+        $("li.nav-item-0 a").text("aiosql 2.0.2 documentation");
     }
 });
